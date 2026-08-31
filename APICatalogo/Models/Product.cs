dff7c6ec9372742f6a19c5b001dd287dc5b8f009
VitorMeloDev <vitormelodev@gmail.com>
2026-08-31 16:21:30 -0300

    Add Swagger and Scalar UI to the project

---
+++ APICatalogo/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace APICatalogo.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? ImageUrl { get; set; }
+    public int Stock { get; set; }
+    public DateTime DateCreated { get; set; }
+}
